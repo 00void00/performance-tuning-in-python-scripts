2d5a19e49630f968114f5dda0937ba255442037f
00void00 <63497724+00void00@users.noreply.github.com>
2020-11-22 14:47:08 +0530

    Update dailysync.py

--- dailysync.py
+++ dailysync.py
@@ -3,12 +3,12 @@ import subprocess
 from multiprocessing import Pool
 import os
 
-def backup(src)
+def backup(src):
         dest = os.getcwd() + "/data/prod_backup/"
         print("Backing up () into ()".format(src, dest))
         subprocess.call(["rsync", "-arq", src, dest])
 
-if __name__ == "__main__";
+if __name__ == "__main__":
         src = os.getcwd() + "/data/prod/"
         list_of_files = os.listdir(src)
         all_files = []
@@ -17,5 +17,5 @@ if __name__ == "__main__";
                 full_path = os.path.join(src, value)
                 all_files.append(full_path)
 
-        with pool(len(all_files)) as pool:
+        with Pool(len(all_files)) as pool:
                 pool.map(backup, all_files)
